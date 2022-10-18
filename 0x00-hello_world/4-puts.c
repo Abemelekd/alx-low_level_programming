fdfe91ca1adf878075af7bcf8a61968783c762db
Abemelek Debebe <abemelekdebebe25@gmail.com>
2022-10-18 04:10:48 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+nclude <stdio.h>
+/**
+ *  * main-entry point
+ *   *
+ *    * Return: 0
+ *     */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
