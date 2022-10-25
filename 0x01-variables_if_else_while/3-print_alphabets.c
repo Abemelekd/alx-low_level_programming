3b980a6d5d2f5208a924bd0571bda35277ebc69c
Abemelek Debebe <abemelekdebebe25@gmail.com>
2022-10-25 01:47:14 -0700

    abc

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *  * main-entry point
+ *   *
+ *    * Return: 0
+ *     */
+int main(void)
+{
+		char alphabet;
+			char upper_case;
+
+				upper_case = 'A';
+					alphabet = 'a';
+
+
+						while (alphabet <= 'z')
+								{
+											putchar(alphabet);
+													alphabet++;
+														}
+							while (upper_case <= 'Z')
+									{
+												putchar(upper_case);
+														upper_case++;
+															}
+								putchar('\n');
+									return (0);
+}
