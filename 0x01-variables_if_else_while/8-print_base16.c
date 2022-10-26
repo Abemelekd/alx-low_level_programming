551c12a5c7c4138980d4ec369ef1b9f1b5ffa0ac
Abemelek Debebe <abemelekdebebe25@gmail.com>
2022-10-25 23:41:04 -0700

    dsf

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+nclude<stdio.h>
+/**
+ *  * main-entry point
+ *   *
+ *    * Return: 0
+ *     */
+int main(void)
+{
+		int n;
+			char letter;
+
+				for (n = 0; n <= 9; n++)
+							putchar((n % 10) + '0');
+					for (letter = 'a'; letter <= 'f'; letter++)
+								putchar(letter);
+
+						putchar('\n');
+							return (0);
+}
