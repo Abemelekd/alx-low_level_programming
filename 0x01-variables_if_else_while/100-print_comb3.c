3ec64044f788619a4613cf1bd33cdb3cc2655af1
Abemelek Debebe <abemelekdebebe25@gmail.com>
2022-10-25 23:48:05 -0700

    adv

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *  * main-entry point
+ *   *
+ *    *Return: always 0
+ *     */
+
+int main(void)
+{
+		int a, b;
+
+			for (a = 0; a < 10; a++)
+					{
+								for (b = 0; b < 10; b++)
+											{
+															if (a != b && a < b)
+																			{
+																							putchar(a + '0');
+																										putchar(b + '0');
+																													if (a != 8)
+																																	{
+																																						putchar(',');
+																																										putchar(' ');
+																																													}
+																																}
+																	}
+									}
+				putchar('\n');
+					return (0);
+}
