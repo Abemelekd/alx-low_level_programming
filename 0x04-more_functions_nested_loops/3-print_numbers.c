bca363e2eedba3695a3936da47b87e184b9ca0e1
Abemelek Debebe <abemelekdebebe25@gmail.com>
2022-10-27 00:47:37 -0700

    sdf

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,16 @@
 #include "main.h"
-
 /**
- *  * main - check the code
+ *  * print_numbers - print numbers 0 to 9
  *   *
- *    * Return: Always 0.
+ *    * Return: none
  *     */
-int main(void)
+void print_numbers(void)
 {
-	    print_numbers();
-	        return (0);
+		int n;
+
+		for (n = 0; n <= 9; n++)
+		{
+				_putchar(n + '0');
+		}
+			_putchar('\n');
 }
